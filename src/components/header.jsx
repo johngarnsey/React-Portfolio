e0b9d356167857f5d1188ebcd39b1bd9fbfc557f
johngarnsey <johngarnsey@hotmail.com>
2025-02-19 16:12:40 -0500

    Fix issue with the header routes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/header.css';
 
 function Header() {
   return (
     <header className="header">
       <nav className="navbar">
-        <div className="logo">React-Portfolio</div>
+        <div className="logo">react-portfolio</div>
         <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
