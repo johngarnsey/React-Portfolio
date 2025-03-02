5dda0809e9ee4371f32ab7c1cd710dcced0df17d
johngarnsey <johngarnsey@hotmail.com>
2025-03-01 23:04:38 -0500

    Adjusting header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/header.css';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header">
-      <nav>
+      {/* Brand Name / Logo */}
+      <div className="brand">
+        <Link to="/">GARNSEY</Link>
+      </div>
+
+      {/* Hamburger Icon */}
+      <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+
+      {/* Navigation Menu */}
+      <nav className={`nav-menu ${isOpen ? 'open' : ''}`}>
         <ul>
-          <li>
-            <Link to="/" data-aos="fade-down">Home</Link>
-          </li>
-          <li>
-            <Link to="/about" data-aos="fade-down" data-aos-delay="100">About</Link>
-          </li>
-          <li>
-            <Link to="/projects" data-aos="fade-down" data-aos-delay="200">Projects</Link>
-          </li>
-          <li>
-            <Link to="/contact" data-aos="fade-down" data-aos-delay="200">Contact</Link>
-          </li>
+          <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+          <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
+          <li><Link to="/projects" onClick={toggleMenu}>Projects</Link></li>
+          <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
         </ul>
       </nav>
     </header>
